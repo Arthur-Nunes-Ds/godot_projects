72f09bdf4c827fee340665ed063831499db47ee1
nunes <arthurnunescarvalho2@gmail.com>
2025-06-07 09:45:27 -0300

    add movimentação no plaey e configuração inicial do scripts

---
+++ fight_random/script/globals.gd
@@ -0,0 +1,15 @@
+extends Node
+#esse arquivo só exite como um meio para compartilhar vars entres código
+#assim facilidanto o desenvolvimento
+
+#vars
+#boneco/player1:
+var vida_boneco1: int
+var boneco_slect1: String
+var special_boneco1: int
+#boneco/player2:
+var vida_boneco2: int
+var special_boneco2: int
+var boneco_slect2: String
+#boneco/player que ganhou o roud
+var boneco_win: String
