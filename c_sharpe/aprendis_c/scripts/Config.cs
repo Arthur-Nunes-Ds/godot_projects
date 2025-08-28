d6c69e653b9979322fc1acffaab219af1b9522cd
nunes <arthurnunescarvalho2@gmail.com>
2025-08-27 23:42:25 -0300

    add save das config via json

--- c_sharpe/aprendis_c/scripts/Config.cs
+++ c_sharpe/aprendis_c/scripts/Config.cs
@@ -1,12 +1,24 @@
 using Godot;
-using System;
+
+//necessario para acessar arquivos
+using System.IO;
+//necessario para o encondig
+using System.Text;
+
+//curta um comando grande 
+using Dictionary = Godot.Collections.Dictionary;
 
 public partial class Config : Control
 {
-    //var's:
-    public bool is_voltar = false;
-    public int id_resolucao = 0;
-
+    //ANCHOR - var's:
+    bool is_voltar = false;
+    int id_resolucao = 0;
+    //dic
+    Dictionary user_save = new Dictionary();
+    //pega o caminho onde o jogo salva ás coisa do player local(como um ".mine...")
+    string USER_PATCH = ProjectSettings.GlobalizePath("user://");
+    
+    //ANCHOR - @exporte
     [Export]
     public string Cenes;
 
@@ -16,25 +28,44 @@ public partial class Config : Control
     [Export]
     public Button Volta, Aplcar;
 
+    //ANCHOR - funções 
     public override void _Ready()
     {
         //faz o linck de metetos
         Volta.Pressed += voltar;
         Aplcar.Pressed += aplicar_btn;
         resolucao.ItemSelected += reslocao_indexe;
-
-        video();
+        //GD.Print(USER_PATCH); //<- descobrir o caminho do user
+        load();
         base._Ready();
     }
+    private void load()
+    {
+        string jsl = LoadJson(USER_PATCH, "config_user.json");
+        //cira um obj json
+        Json jsonCaregand = new Json();
+        //caso der algum erro ná ora de converte para json->dic
+        Error error = jsonCaregand.Parse(jsl);
+        if (error != Error.Ok) { GD.Print(error); return; }
+        //se nada da errado ele passa apra o formato dic da godot
+        user_save = (Dictionary)jsonCaregand.Data;
+
+        //pega o valor dentro do dic data(json) na key[opcao_reolucao] é já converte para int 
+        id_resolucao = (int)user_save["opcao_reolucao"];
+
+        video();
+    }
+
     //config video:
-    int video(){
+    private void video()
+    {
+        //defalte resolução / criação de vector2I
+        Vector2I reso = new Vector2I(1152, 648);
         //resolução
-            //defalte resolução / criação de vector2I
-        Vector2I reso = new Vector2I(1152,648);
         //match case
         switch (id_resolucao)
         {
-            case 0:
+            case 0://<- opção defalt 
                 //edita um vector2I
                 reso = new Vector2I(1152, 648);
                 //conserta a opção do index
@@ -53,11 +84,12 @@ public partial class Config : Control
                 resolucao.Select(3);
                 break;
         }
+
         //seta a resolução
         DisplayServer.WindowSetSize(reso);
-        return 0;
     }
 
+    //ANCHOR - conexção de metedos da godot
     private void voltar()
     {
         GD.Print("voltar");
@@ -66,18 +98,32 @@ public partial class Config : Control
         aplicar_btn();
     }
     private void aplicar_btn()
-    {   
+    {
+        //faz o manda o para o sjon salvar certinho
+        void salvar()
+        {
+            //supstuir no dic | dic[key] = vaule
+            user_save["opcao_reolucao"] = id_resolucao;
+
+            //converte sjon para uma espece de string
+            string user_save_str = Json.Stringify(user_save);
+            SaveJson(USER_PATCH, user_save_str, "config_user.json");
+            return;
+        }
+
         if (is_voltar == true)
         {
-            GD.Print("aplicar");
-            video();
+            //GD.Print("aplicar -> tela scream");
+            salvar();
+            load();
             //mudar cena
             GetTree().ChangeSceneToFile(Cenes);
         }
         else
         {
-            video();
-            GD.Print("aplicar");
+            //GD.Print("aplicar -> salvo");
+            salvar();
+            load();
         }
     }
 
@@ -85,7 +131,51 @@ public partial class Config : Control
     private void reslocao_indexe(long index)
     {
         id_resolucao = (int)index;
-        //contenção tipo f string no python
-        GD.Print($"Index: {id_resolucao}");
+        /*contenção tipo f string no python
+        GD.Print($"Index: {id_resolucao}");*/
+    }
+
+    //ANCHOR manibulção do json atraves da GODOT
+    //salvar sjon
+    private void SaveJson(string caminhoSjon, string dic, string nome_json)
+    {
+        //para ir até o caminho acima e abri o json
+        caminhoSjon = Path.Join(caminhoSjon, nome_json);
+
+        //try expect
+        try
+        {
+            //sobre escreve o arquivo (caminho, dicinorio, encode caso aja)     
+            File.WriteAllText(caminhoSjon, dic, Encoding.UTF8);
+        }
+        catch (System.Exception e)
+        {
+            GD.Print(e);
+        }
+
+        return;
+    }
+
+    //loadejson
+    private string LoadJson(string caminhoSjon, string nome_json)
+    {
+        string dados = null;
+
+        caminhoSjon = Path.Join(caminhoSjon, nome_json);
+
+        //ver ser o json existe
+        if (!File.Exists(caminhoSjon)) return null;
+
+        try
+        {
+            //ler
+            dados = File.ReadAllText(caminhoSjon, Encoding.UTF8);
+        }
+        catch (System.Exception e)
+        {
+            GD.Print(e);
+        }
+        
+        return dados;
     }
 }
